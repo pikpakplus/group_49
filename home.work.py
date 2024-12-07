f3fc684aa23dc3b67dd43e38bb89bbe082733a0e
pikpakplus <pikpakplus@gmail.com>
2024-12-07 21:54:07 +0600

    hw2

---
+++ home.work.py
@@ -0,0 +1,68 @@
+# Операторы: принадлежности, назначения. Циклы.
+
+# word = 'KYRGYZSTAN'
+# #i - item, iterable, index
+#
+# for letter in word:
+#     if letter == 'S':
+#         break
+#     #if letter == 'R' or letter == 'Z':
+#     if letter in 'RZ':
+#         continue
+#     print(letter)
+
+
+
+# in_lesson = 0
+# not_in_lesson = 0
+# students = 20
+#
+# for number in range(1, 21):
+#     answer = int(input(f'студент под номером №{number}: '))
+#     if answer == 1:
+#         in_lesson += 1
+#     else:
+#         not_in_lesson += 1
+#
+# print(f'кол-во студентов: {students}\n'
+#       f'присутствует: {in_lesson}\n'
+#       f'отсутствует: {not_in_lesson}')
+
+
+
+
+
+ф
+
+
+
+
+
+
+
+# numbers = range(1, 11)
+# print(4 in numbers)
+# print(24 in numbers)
+#
+# word = 'geeks'
+# print('g' in word)
+# print('o' in word)
+
+# number = 5
+# print(number)
+# number += 4
+# number /= 2
+# number **= 2
+# number -= 0.25
+# number %= 2
+# print(number)
+
+# rounds = 0
+# while rounds < 100:
+#     rounds += 1
+#     if rounds == 50:
+#         print('exit...')
+#         break
+#     if rounds % 2 == 0:
+#         continue
+#     print('Hello, world!', rounds)
---
+++ hw.py
@@ -0,0 +1,55 @@
+#
+# name = 'azamat'
+# surname = "kadyrov"
+#
+#
+#
+# print(10 * 5)
+#
+# sumage = 16 + 22 + 19 + 15 + 16 + 15 + 18 + 14 + 17 + 19 + 18 + 17 + 18 + 26 + 19 + 22
+# print(sumage)
+# student_count = 16
+# average = sumage / student_count
+# average = round(average, 1)
+# print(f'средний возраст группы 49-1: {average}')h
+
+
+
+
+
+
+# name = input('what is your name? ').title()
+# surname = "o'neil"
+# age = int(input(f'{name}, please, enter your age: '))
+# height = 1.8
+# current_year = 2024
+# born = current_year - age
+#
+# print(f'name: {name} surname: {surname} age: {age} born: {born}')
+
+
+
+
+
+
+
+
+
+
+
+# print(type(name))
+# print(type(surname))
+#
+
+
+
+
+
+
+
+
+
+
+
+
+# print('hello world')
